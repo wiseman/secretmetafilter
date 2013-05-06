3207fb0856f656c54eea0b355249d9817d11ad2a
John Wiseman <jwiseman@morseproject.com>
2013-05-06 13:37:58 -0700

    Fixed CSS switching to work in Firefox, too.

--- secretmefi/static/app.js
+++ secretmefi/static/app.js
@@ -1,6 +1,7 @@
 goog.provide('SecretMefi.App');
 
 goog.require('goog.net.cookies');
+goog.require('goog.dom');
 
 /**
  * SecretMefi App object.
@@ -15,13 +16,13 @@ SecretMefi.App = function() {
  * Initializes and starts the app.
  */
 SecretMefi.App.prototype.start = function() {
-  var colorElement = document.getElementById('color');
+  var colorElement = document.getElementById("color");
   colorElement.addEventListener(
     "change",
-    goog.bind(this.colorChanged, this),
+    goog.bind(this.colorChanged_, this),
     false);
 
-  var colorPref = goog.net.cookies.get('color');
+  var colorPref = goog.net.cookies.get("color", "blue");
   if (colorPref) {
     this.setColor(colorPref);
     colorElement.value = colorPref;
@@ -29,27 +30,33 @@ SecretMefi.App.prototype.start = function() {
 };
 
 SecretMefi.App.prototype.setColor = function(color) {
-  if (color === "blue") {
-    document.getElementById("white-css").setAttribute(
-      "disabled", "disabled");
-    document.getElementById("blue-css").removeAttribute(
-      "disabled");
+  var cssUrl = null;
+  if (color === "white") {
+    cssUrl = "/static/white.css";
   } else {
-    document.getElementById("blue-css").setAttribute(
-      "disabled", "disabled");
-    document.getElementById("white-css").removeAttribute(
-      "disabled");
+    cssUrl = "/static/blue.css";
   }
+  var dh = new goog.dom.DomHelper();
+  window.console.log(dh);
+  dh.getElementsByTagNameAndClass("head")[0].appendChild(
+    goog.dom.createDom(
+      "link",
+      {
+        "rel": "stylesheet",
+        "type": "text/css",
+        "href": cssUrl
+      }));
 };
 
-SecretMefi.App.prototype.colorChanged = function(e) {
-  var colorPref = e.target['value'];
+SecretMefi.App.prototype.colorChanged_ = function(e) {
+  var colorPref = e.target["value"];
   this.setColor(colorPref);
-  goog.net.cookies.set('color', colorPref);
+  goog.net.cookies.set("color", colorPref);
 }
 
 
 
 // Ensures the symbol will be visible after compiler renaming.
-goog.exportSymbol('SecretMefi.App', SecretMefi.App);
-goog.exportSymbol('SecretMefi.App.prototype.start', SecretMefi.App.prototype.start);
+goog.exportSymbol("SecretMefi.App", SecretMefi.App);
+goog.exportSymbol("SecretMefi.App.prototype.start",
+                  SecretMefi.App.prototype.start);
